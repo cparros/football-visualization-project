e7bef2f8c09fb7ff9198ac93c3d5eb6d94ede896
cparros <celparros@gmail.com>
2021-04-14 10:21:02 -0400

    separated JS

---
+++ public/js/derekGraph.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+  var xmlHttp = new XMLHttpRequest();
+  var url = "http://127.0.0.1:5500/data/derekCarrData.json";
+  xmlHttp.open("GET", url, true);
+  xmlHttp.send();
+  xmlHttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var data = JSON.parse(this.responseText);
+      //  console.log(data)
+
+      var week = data.map(function (game) {
+        return game.week;
+      });
+
+      var yards = data.map(function (game) {
+        return game.PsYds;
+      });
+
+      var comp = data.map(function (game) {
+        return game.Cmp;
+      });
+
+      var att = data.map(function (game) {
+        return game.Att;
+      });
+
+
+      var ctx = $("#baker-chart");
+
+      window.myChart = new Chart(ctx, {
+        type: "line",
+        data: {
+          labels: week,
+          datasets: [
+            {
+              label: "Derek Carr Yards Per Game/Week",
+              data: yards,
+              backgroundColor: ["red"],
+              borderColor: ["red"],
+              borderWidth: 3,
+              tension: 0.2,
+            },
+            {
+              label: "Derek Carr Completions Per Game/Week",
+              data: comp,
+              backgroundColor: ["green"],
+              borderColor: ["green"],
+              borderWidth: 3,
+              tension: 0.2,
+            },
+            {
+              label: "Derek Carr Pass Attempts Per Game/Week",
+              data: att,
+              backgroundColor: ["orange"],
+              borderColor: ["orange"],
+              borderWidth: 3,
+              tension: 0.2,
+            },
+          ],
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true,
+              },
+            },
+          },
+        },
+      });
+    }
+  };
+});
---
+++ public/js/homeTable.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+  function getBakerData() {
+    $.getJSON("./data/bakerMayfieldData.json", function (data) {
+      var game = "";
+      $.each(data, function (key, value) {
+        game += "<tr>";
+        game += "<td>" + value.week + "</td>";
+        game += "<td>" + value.opponent + "</td>";
+        game += "<td>" + value.Att + "</td>";
+        game += "<td>" + value.Cmp + "</td>";
+        game += "<td>" + value.Sack + "</td>";
+        game += "<td>" + value.Int + "</td>";
+        game += "<td>" + value.PsYds + "</td>";
+        game += "<td>" + value.PsTD + "</td>";
+        game += "<td>" + value.Rush + "</td>";
+        game += "<td>" + value.RshYds + "</td>";
+        game += "<td>" + value.RshTD + "</td>";
+      });
+
+      //INSERTING ROWS INTO TABLE
+      $("#table").append(game);
+    });
+  }
+  getBakerData();
+
+  function getBlakeData() {
+    $.getJSON("./data/blakeBortlesData.json", function (data) {
+      var game = "";
+      $.each(data, function (key, value) {
+        game += "<tr>";
+        game += "<td>" + value.week + "</td>";
+        game += "<td>" + value.opponent + "</td>";
+        game += "<td>" + value.Att + "</td>";
+        game += "<td>" + value.Cmp + "</td>";
+        game += "<td>" + value.Sack + "</td>";
+        game += "<td>" + value.Int + "</td>";
+        game += "<td>" + value.PsYds + "</td>";
+        game += "<td>" + value.PsTD + "</td>";
+        game += "<td>" + value.Rush + "</td>";
+        game += "<td>" + value.RshYds + "</td>";
+        game += "<td>" + value.RshTD + "</td>";
+      });
+
+      //INSERTING ROWS INTO TABLE
+      $("#tabletwo").append(game);
+    });
+  }
+  getBlakeData();
+
+  function getDerekData() {
+    $.getJSON("./data/derekCarrData.json", function (data) {
+      var game = "";
+      $.each(data, function (key, value) {
+        game += "<tr>";
+        game += "<td>" + value.week + "</td>";
+        game += "<td>" + value.opponent + "</td>";
+        game += "<td>" + value.Att + "</td>";
+        game += "<td>" + value.Cmp + "</td>";
+        game += "<td>" + value.Sack + "</td>";
+        game += "<td>" + value.Int + "</td>";
+        game += "<td>" + value.PsYds + "</td>";
+        game += "<td>" + value.PsTD + "</td>";
+        game += "<td>" + value.Rush + "</td>";
+        game += "<td>" + value.RshYds + "</td>";
+        game += "<td>" + value.RshTD + "</td>";
+      });
+
+      //INSERTING ROWS INTO TABLE
+      $("#tablethree").append(game);
+    });
+  }
+  getDerekData();
+});
